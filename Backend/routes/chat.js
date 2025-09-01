e5ac8c6f7a13c04ce18314d95cdfb0e7fac37fa3
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-09-01 21:29:18 +0530

    installed openAi

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,8 +1,22 @@
 import express from "express";
+import OpenAI from 'openai';
+
 const chatRoute = express.Router();
+const client = new OpenAI();
 
 chatRoute.post("/start", (req, res) => {
-  
+  const { query } = req.body;
+  if (!query) {
+    return res.status(400).json({ error: "Message field is required" });
+  }
+  const formattedUserQuery = query.trim().toLowerCase();
+
+  res.status(200).send({
+    success: true,
+    message: formattedUserQuery,
+  });
+
+
 });
 
 export default chatRoute;
