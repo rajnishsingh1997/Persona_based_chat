495f0c64f7fb2d4ec835454e0cc040dc30bb53b2
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-09-01 20:39:23 +0530

    added routes for the chat and initial setup

---
+++ Backend/routes/chat.js
@@ -0,0 +1,8 @@
+import express from "express";
+const chatRoute = express.Router();
+
+chatRoute.post("/start", (req, res) => {
+  
+});
+
+export default chatRoute;
---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import chatRoute from "./routes/chat.js";
+
+const app = express();
+const port = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+app.use("/chat", chatRoute);
+
+app.listen(port, () => {
+  console.log("server working");
+});
