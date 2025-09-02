9c592f6998c20e0d2efeee17977d479b802bb1ef
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-09-02 06:58:59 +0530

    added tanstack

--- Frontend/src/components/ChatScreen.jsx
+++ Frontend/src/components/ChatScreen.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import { useQuery } from '@tanstack/react-query';
 
 const ChatScreen = () => {
+  const [userQuery, setUserQuery] = useState("");
+
+  const handleInputUserQuery = (e) => {
+    setUserQuery(e.target.value);
+  };
+
+  // we need to define the function that can be passed to the tanstack for the api calls.
+
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-3">
       <input
+        onChange={handleInputUserQuery}
         placeholder="Enter your message.."
         className="border-2 border-black px-4 py-2 rounded w-72"
       />
