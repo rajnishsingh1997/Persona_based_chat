6f30d9fbb1995410f051ab3a29701633cb0d5e33
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-09-02 06:25:13 +0530

    added open ai and made the api call using the query provided

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,22 +1,30 @@
 import express from "express";
-import OpenAI from 'openai';
+import OpenAI from "openai";
 
 const chatRoute = express.Router();
 const client = new OpenAI();
 
-chatRoute.post("/start", (req, res) => {
+chatRoute.post("/start", async (req, res) => {
   const { query } = req.body;
   if (!query) {
     return res.status(400).json({ error: "Message field is required" });
   }
   const formattedUserQuery = query.trim().toLowerCase();
 
-  res.status(200).send({
-    success: true,
-    message: formattedUserQuery,
+  const completion = await client.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "user",
+        content: formattedUserQuery,
+      },
+    ],
   });
 
-
+  res.status(200).send({
+    success: true,
+    message: completion.choices[0].message.content,
+  });
 });
 
 export default chatRoute;
