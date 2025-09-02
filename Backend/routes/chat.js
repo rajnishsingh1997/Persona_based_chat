23167e76ab6252a854c701d9dda29317e2f062ce
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2025-09-02 18:21:08 +0530

    added the logic to give the chat history

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -4,27 +4,42 @@ import OpenAI from "openai";
 const chatRoute = express.Router();
 const client = new OpenAI();
 
+let messages = [
+  {
+    role: "system",
+    content: `you are a ai chatbot designed to answer javascript question}`,
+  },
+];
+
 chatRoute.post("/start", async (req, res) => {
   const { query } = req?.body;
   if (!query || query === "undefined") {
     return res.status(400).json({ error: "Message field is required" });
   }
   const formattedUserQuery = query.trim().toLowerCase();
+
+  messages.push({ role: "user", content: formattedUserQuery });
+
+  // let newMessages = [
+  //   ...messages,
+  //   { role: "user", content: formattedUserQuery },
+  // ];
+
   try {
     const completion = await client.chat.completions.create({
       model: "gpt-4o-mini",
-      messages: [
-        {
-          role: "user",
-          content: formattedUserQuery,
-        },
-      ],
+      messages: messages,
     });
 
     if (!completion) {
       throw new Error("Failed to make the api call");
     }
 
+    messages.push({
+      role: "assistant",
+      content: completion.choices[0].message.content,
+    });
+    console.log(messages);
     res.status(200).send({
       success: true,
       message: completion.choices[0].message.content,
